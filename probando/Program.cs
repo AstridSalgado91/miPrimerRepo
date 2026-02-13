f719cbaa0114a45b10103071dbc06b3a94578023
Astrid Salgado <mashisalher@gmail.com>
2026-02-13 13:52:15 -0600

    Cadena nueva

--- probando/Program.cs
+++ probando/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, Itza!");
 
 string stripe_api_key = "sk_test_4eC39HqLyjWDarYgLhQy00mLJtT2X00Zzqj8s1";
 //brecha de seguridad, no se debe compartir la clave de la API en el código fuente, se recomienda usar variables de entorno o un archivo de configuración seguro. yse ingresa en el json
+
+string testingConnection = "Server=localhost;Database=prueba;User Id=sa;Password=yourpassword;";
