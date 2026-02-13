9d71973e74b3fa0f825564da73a898ab8d6248c8
Astrid Salgado <mashisalher@gmail.com>
2026-02-13 13:47:43 -0600

    Nuevos cambios

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Itza!");
 
-string stripe_api_key = "sk_test_51N8n9uKj2mLh7sXo5l3a1Zt3v";
+string stripe_api_key = "sk_test_4eC39HqLyjWDarYgLhQy00mLJtT2X00Zzqj8s1";
 //brecha de seguridad, no se debe compartir la clave de la API en el código fuente, se recomienda usar variables de entorno o un archivo de configuración seguro. yse ingresa en el json
